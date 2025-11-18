9fd353e294df32b921de92aa3fabf43306b10192
jaga2835-hue <jaga2835@gmail.com>
2025-11-18 22:12:39 +0530

    Fix NetBankingBasicTest.java - Add missing By import

---
+++ src/test/java/tests/NetBankingBasicTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.BaseTest;
+import pages.PaymentPage;
+
+public class NetBankingBasicTest extends BaseTest {
+
+    private PaymentPage paymentPage;
+
+    @BeforeMethod
+    public void setup() {
+        paymentPage = new PaymentPage(driver);
+    }
+
+    @Test
+    public void openPageAndClickNetBanking() throws Exception {
+
+        System.out.println("=== Opening TimesPrime Payment Page ===");
+
+        // Step 1: Open the payment page URL
+        driver.get("https://sandbox.assets.juspay.in/payment-page/signature/picasso-019a97d01dc3000000000000ecb1edf8");
+
+        // wait to load page fully (reduce to 5 sec)
+        Thread.sleep(5000);
+
+        System.out.println("=== Clicking NetBanking Option ===");
+
+        // Step 2: Click NetBanking on left sidebar
+        // Use this XPath → //*[@data-testid='netbanking']
+        driver.findElement(By.xpath("//*[@data-testid='netbanking']")).click();
+
+        System.out.println("=== NetBanking Clicked Successfully ===");
+    }
+}
